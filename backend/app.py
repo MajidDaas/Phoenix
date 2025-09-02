b274b6351405d3800e7bd645d847ff90c435576c
MajidDaas <gman.gm725@gmail.com>
2025-09-03 02:08:50 +0300

    0.22 votes to csv

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_from_directory, session, redirect, url_for
+from flask import Flask, jsonify, request, send_from_directory, session, redirect, url_for, Response
 from flask_cors import CORS
 import io
 import csv
